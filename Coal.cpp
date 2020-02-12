f3de916868c900d016315ed3475177ae1623842b
Hlanetest <31164243+Hlanetest@users.noreply.github.com>
2020-02-11 23:21:21 -0500

    Update Coal.cpp

--- Coal.cpp
+++ Coal.cpp
@@ -10,11 +10,9 @@ const int CARS_WEIGHT = 74.5;
 int main() {
     string RAIL_NAME, DESTINATION, SURCHARGE, line;
     int NUM_CARS;
-    int TON_COST;
+    float TON_COST, TOTAL_VAL, TOTAL_WEIGHT, SURCHARGE_EST;
     ifstream ORE_SHIPPING("/home/hadrian/Desktop/Railway1.txt");
-    
     getline(ORE_SHIPPING, RAIL_NAME);
-    cin.ignore();
     ORE_SHIPPING >> NUM_CARS >> TON_COST;
     getline(ORE_SHIPPING, DESTINATION);
     getline(ORE_SHIPPING, SURCHARGE);
@@ -22,8 +20,13 @@ int main() {
     //ORE_SHIPPING >> RAIL_NAME >> NUM_CARS >> TON_COST >> DESTINATION >> SURCHARGE;
     cout << NUM_CARS << "\n";
     cout << TON_COST << "\n";
+    cout << DESTINATION << "\n";
     
-    cout << SURCHARGE << "\n";
+    //The equations to get the numbers we need for later.
+    TOTAL_WEIGHT = NUM_CARS * CARS_WEIGHT;
+    TOTAL_VAL = TOTAL_WEIGHT * TON_COST;
+    SURCHARGE_EST = TOTAL_VAL * SURCHARGE_COST;
+
     return 0;
 }
 http://www.cplusplus.com/reference/string/string/getline/
